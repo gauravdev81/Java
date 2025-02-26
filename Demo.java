6b3973ab1935af97bf6df78fa16e12a99b74d36c
Gaurav <gaurav.dev@emsibg.com>
2025-02-26 13:12:07 +0530

    initial files added

---
+++ Demo.java
@@ -0,0 +1,18 @@
+class Calculator{
+    public int add(int n1, int n2){
+        int result = n1+n2;
+        return result;
+    }
+}
+
+class Demo{
+    public static void main(String[] args){
+        int num1 = 10;
+        int num2 = 20;
+        
+        //creating an object "c" of class Calculator
+        Calculator c = new Calculator();
+        int result = c.add(num1,num2);
+        System.out.println(result);
+    }
+}
